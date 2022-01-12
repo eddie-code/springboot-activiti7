923537c87e75edc947035eeaffc65978189be4c7
eddie-code <231071372@qq.com>
2022-01-12 15:51:48 +0800

    4.4 流程部署 - Deployment

---
+++ src/test/java/com/edcode/activiti/ActivityApplicationTest.java
@@ -0,0 +1,17 @@
+package com.edcode.activiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author eddie.lee
+ * @description
+ */
+@SpringBootTest
+class ActivityApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/edcode/activiti/Part1_Deployment.java
@@ -0,0 +1,70 @@
+package com.edcode.activiti;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.InputStream;
+import java.util.zip.ZipInputStream;
+
+/**
+ * @author eddie.lee
+ * @description 流程部署Deployment
+ *
+ *  mysql tables : ACT_RE_DEPLOYMENT、ACT_GE_BYTEARRAY
+ *
+ */
+@SpringBootTest
+public class Part1_Deployment {
+
+    @Autowired
+    private RepositoryService repositoryService;
+
+    /**
+     * 通过bpmn部署流程
+     */
+    @Test
+    public void initDeploymentBPMN() {
+        String filename = "BPMN/Part1_Deployment.bpmn20.xml";
+//        String pngname="BPMN/Part1_Deployment.png";
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource(filename)
+//                .addClasspathResource(pngname)
+                .name("流程部署测试BPMN_v2")
+                .deploy();
+        System.out.println(deployment.getName());
+    }
+
+    /**
+     * 通过ZIP部署流程
+     */
+    @Test
+    public void initDeploymentZIP() {
+        InputStream fileInputStream = this.getClass()
+                .getClassLoader()
+                .getResourceAsStream("BPMN/Part1_DeploymentV2.zip");
+        assert fileInputStream != null;
+        ZipInputStream zip = new ZipInputStream(fileInputStream);
+        Deployment deployment = repositoryService.createDeployment()
+                .addZipInputStream(zip)
+                .name("流程部署测试zip")
+                .deploy();
+        System.out.println(deployment.getName());
+    }
+
+    /**
+     * 查询流程部署
+     */
+    @Test
+    public void getDeployments() {
+        repositoryService.createDeploymentQuery().list().forEach(deployment -> {
+            System.out.println("Id：" + deployment.getId());
+            System.out.println("Name：" + deployment.getName());
+            System.out.println("DeploymentTime：" + deployment.getDeploymentTime());
+            System.out.println("Key：" + deployment.getKey() + "\n\r");
+        });
+    }
+
+}
