bb3f852a0a6a3647360aaa60ad113d856d796143
eddie-code <231071372@qq.com>
2022-01-17 13:48:36 +0800

    5.1 API新特性ProcessRuntime

---
+++ src/main/java/com/edcode/activiti/DemoApplicationConfiguration.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2010-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.edcode.activiti;
+
+import static java.util.Arrays.asList;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * https://github.com/Activiti/Activiti
+ *
+ * activiti-examples/activiti-api-basic-full-example-bean/src/main/java/org/activiti/examples/DemoApplicationConfiguration.java
+ * 复制过来的
+ *
+ */
+@Configuration
+public class DemoApplicationConfiguration {
+
+    private Logger logger = LoggerFactory.getLogger(DemoApplicationConfiguration.class);
+
+    /**
+     * 硬编码：
+     * 结合上面，就是项目启动的时候，创建上面数组用户，存入内存里面
+     * @return inMemoryUserDetailsManager
+     */
+    @Bean
+    public UserDetailsService myUserDetailsService() {
+
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+
+        // 1用户名、2用户密码、3用户角色、4用户组
+        String[][] usersGroupsAndRoles = {
+                {"bob", "password", "ROLE_ACTIVITI_USER", "GROUP_activitiTeam"},
+                // 修改 bajie 和 wukong
+                {"bajie", "password", "ROLE_ACTIVITI_USER", "GROUP_activitiTeam"},
+                {"wukong", "password", "ROLE_ACTIVITI_USER", "GROUP_activitiTeam"},
+                {"other", "password", "ROLE_ACTIVITI_USER", "GROUP_otherTeam"},
+                {"system", "password", "ROLE_ACTIVITI_USER"},
+                {"admin", "password", "ROLE_ACTIVITI_ADMIN"},
+        };
+
+        for (String[] user : usersGroupsAndRoles) {
+            List<String> authoritiesStrings = asList(Arrays.copyOfRange(user, 2, user.length));
+            logger.info("> Registering new user: " + user[0] + " with the following Authorities[" + authoritiesStrings + "]");
+            inMemoryUserDetailsManager.createUser(new User(user[0], passwordEncoder().encode(user[1]),
+                    authoritiesStrings.stream().map(s -> new SimpleGrantedAuthority(s)).collect(Collectors.toList())));
+        }
+
+        return inMemoryUserDetailsManager;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
