62ec40097a23d0692bb47cd1fde1cc308d3ba3bb
eddie-code <231071372@qq.com>
2022-01-28 15:22:18 +0800

    jar 与 war

--- src/main/java/com/edcode/activiti/ActivityApplication.java
+++ src/main/java/com/edcode/activiti/ActivityApplication.java
@@ -2,16 +2,34 @@ package com.edcode.activiti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author eddie.lee
  * @description
  */
-@SpringBootApplication
-public class ActivityApplication {
+//@SpringBootApplication
+//public class ActivityApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ActivityApplication.class, args);
+//    }
+//
+//}
+
+@SpringBootApplication(scanBasePackages = {"com.edcode.activiti"})
+public class ActivityApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ActivityApplication.class, args);
     }
 
+    /**
+     * 打 war 包方式
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ActivityApplication.class);
+    }
 }
